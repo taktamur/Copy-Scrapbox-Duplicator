5c24fa2d451385a23bf0036e50b294120da6754d
Takashi Tamura <tamura@chaspy.me>
2025-12-28 12:34:39 +0900

    importPagesを500件ごとに分割してインポートするように変更

--- index.ts
+++ index.ts
@@ -41,18 +41,30 @@ if (importingPages.length === 0) {
   console.log("No page to be imported found.");
 } else {
   console.log(
-    `Importing ${importingPages.length} pages to "/${importingProjectName}"...`,
+    `Importing ${importingPages.length} pages to "/${importingProjectName}"...`
   );
-  const result = await importPages(importingProjectName, {
-    pages: importingPages,
-  }, {
-    sid,
-  });
-  if (!result.ok) {
-    const error = new Error();
-    error.name = `${result.value.name} when importing pages`;
-    error.message = result.value.message;
-    throw error;
+  // MEMO: 更新量が大きすぎてのエラーが出たので、暫定で500件ごとに分割してインポートするように変更
+  for (let i = 0; i < importingPages.length; i += 500) {
+    const chunk = importingPages.slice(i, i + 500);
+    console.log(
+      `Importing chunk ${Math.floor(i / 500) + 1} (${chunk.length} pages)...`
+    );
+
+    const result = await importPages(
+      importingProjectName,
+      {
+        pages: chunk,
+      },
+      {
+        sid,
+      }
+    );
+    if (!result.ok) {
+      const error = new Error();
+      error.name = `${result.value.name} when importing pages`;
+      error.message = result.value.message;
+      throw error;
+    }
+    console.log(result.value);
   }
-  console.log(result.value);
 }
